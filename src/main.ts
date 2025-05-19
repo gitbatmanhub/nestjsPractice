48734e7ea2dd20a6ff948c6aa6e8d58762cddd66
EdwinDev <ecro98.98@gmail.com>
2025-05-19 08:08:44 -0500

    Add: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
       forbidNonWhitelisted: false,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Shop Test')
+    .setDescription('Enpoints for testing about nest')
+    .setVersion('1.0')
+    // .addTag('nestjs')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
