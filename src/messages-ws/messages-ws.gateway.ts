2f1521bb6c94e10a84f89031beb8b601d7ccaa90
EdwinDev <ecro98.98@gmail.com>
2025-05-19 18:35:28 -0500

    Add: Receive jwt and resolve data of user Add: validation of token using dependences injection Add: Emit and receive events of server

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,11 +1,15 @@
+import { JwtService } from '@nestjs/jwt';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtPayload } from '../auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
@@ -13,11 +17,23 @@ export class MessagesWsGateway
 {
   @WebSocketServer() wss: Server;
 
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  handleConnection(client: Socket) {
-    this.messagesWsService.registerClient(client);
-    // console.log({ conectados: this.messagesWsService.getConncectedClient() });
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authorization as string;
+    console.log(`Received ${token}`);
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+      // console.log(payload);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
     this.wss.emit(
       'clients-updated',
       this.messagesWsService.getConncectedClient(),
@@ -30,5 +46,28 @@ export class MessagesWsGateway
       'clients-updated',
       this.messagesWsService.getConncectedClient(),
     ); // this.messagesWsService.removeClient(client.id);
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    // console.log(client.id, payload);
+    //Emite al cliente remitente
+    /*client.emit('message-from-server', {
+      fullName: 'Soy yo',
+      message: payload || 'New message from',
+    });*/
+
+    //Emitir a todos menos al cliente remitente
+    /*client.broadcast.emit('message-from-server', {
+      fullName: 'Soy yo',
+      message: payload || 'New message from',
+    });*/
+
+    //Emitir a todos
+
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'New message from',
+    });
   }
 }
